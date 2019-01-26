b65208aecb21b02bdd802c4654c99097cf164533
chrislmy <zcabmyl@ucl.ac.uk>
2019-01-26 19:31:06 +0000

    Updates

---
+++ precache-manifest.c0e63c34a9f380d5a3099d9d675bf1c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e0933abf200cadef9f26bc084f9b74c",
+    "url": "/online-jukebox/static/media/logo.6e0933ab.svg"
+  },
+  {
+    "revision": "42659c6efece450aa56e",
+    "url": "/online-jukebox/static/js/runtime~main.42659c6e.js"
+  },
+  {
+    "revision": "ec178e07d8a8c5af89fd",
+    "url": "/online-jukebox/static/js/main.ec178e07.chunk.js"
+  },
+  {
+    "revision": "63e2a98234d082441a09",
+    "url": "/online-jukebox/static/js/1.63e2a982.chunk.js"
+  },
+  {
+    "revision": "ec178e07d8a8c5af89fd",
+    "url": "/online-jukebox/static/css/main.e55cc500.chunk.css"
+  },
+  {
+    "revision": "a1b8316a637b17ce18f3cd93e197e94d",
+    "url": "/online-jukebox/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/online-jukebox/precache-manifest.f090bba99854176545615733f7c06677.js"
+  "/online-jukebox/precache-manifest.c0e63c34a9f380d5a3099d9d675bf1c6.js"
 );
 
 workbox.clientsClaim();

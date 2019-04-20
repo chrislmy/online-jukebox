79c8e4c517e497c8e64674768cfe99594aa9c44a
chrislmy <zcabmyl@ucl.ac.uk>
2019-04-20 18:50:13 +0100

    volume slider bar

--- src/components/YoutubePlayer/subComponents/videoBanner.js
+++ src/components/YoutubePlayer/subComponents/videoBanner.js
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap/lib';
 import { connect } from 'react-redux';
+import InputRange from 'react-input-range';
 import videoQueueActions from '../../../actions/actionCreators/video-queue-actions';
 import lobbyActions from '../../../actions/actionCreators/lobby-actions';
+import "react-input-range/lib/css/index.css";
 import './videoBanner.css';
-
-const volumes = [25, 50, 75, 100];
 
 const volumeControl = (volume, player) => {
     lobbyActions.updateVolume(volume);
@@ -18,61 +18,49 @@ const volumeControl = (volume, player) => {
     player.unMute();
 }
 
-const VolumeButton = ({volume, player}) => (
-    <Button 
-        className="volume-buttons"
-        bsStyle="primary"
-        bsSize="small"
-        onClick={ () => { volumeControl(volume, player) } }
-    >
-        {volume}
-    </Button>
-);
+const VideoBannerView = ({suggestedUser, player, volume}) => (
+    <React.Fragment>
+        <h4 className="video-banner-title">
+            Added by : <span className="suggested-user" >{suggestedUser}</span>
+        </h4>
+        <div className="control-buttons">
+            <Button 
+                className="skip-video-button"
+                bsStyle="primary"
+                bsSize="small"
+                onClick={ () => videoQueueActions.updateQueue(true) }
+            >
+                Skip <i className="skip-icon fas fa-forward"></i>
+            </Button>
 
-const VideoBannerView = ({suggestedUser, player, volume}) => {
-    const volumeButtons = volumes.map((volume,index) => (
-        <VolumeButton player={player} volume={volume} key={index} />
-    ))
-
-    return (
-        <React.Fragment>
-            <h4 className="video-banner-title">
-                Added by : <span className="suggested-user" >{suggestedUser}</span>
-            </h4>
-            <div className="control-buttons">
+            { volume === 0 ?
                 <Button 
-                    className="skip-video-button"
+                    className="mute-button"
                     bsStyle="primary"
                     bsSize="small"
-                    onClick={ () => videoQueueActions.updateQueue(true) }
+                    onClick={ () => { volumeControl(50, player) } }
                 >
-                    Skip <i className="skip-icon fas fa-forward"></i>
+                    <i className="fas fa-volume-mute"></i>
+                </Button> :
+                <Button 
+                    className="mute-button"
+                    bsStyle="primary"
+                    bsSize="small"
+                    onClick={ () => { volumeControl(0, player) } }
+                >
+                    <i className="fas fa-volume-up"></i>
                 </Button>
-
-                { volume === 0 ?
-                    <Button 
-                        className="mute-button"
-                        bsStyle="primary"
-                        bsSize="small"
-                        onClick={ () => { volumeControl(50, player) } }
-                    >
-                        <i className="fas fa-volume-mute"></i>
-                    </Button> :
-                    <Button 
-                        className="mute-button"
-                        bsStyle="primary"
-                        bsSize="small"
-                        onClick={ () => { volumeControl(0, player) } }
-                    >
-                        <i className="fas fa-volume-up"></i>
-                    </Button>
-                }
-
-                {volumeButtons}
-            </div>
-        </React.Fragment>
-    )
-};
+            }
+            <InputRange
+                maxValue={100}
+                minValue={0}
+                value={volume}
+                onChange={value => volumeControl(value, player)} 
+            />
+        </div>
+        
+    </React.Fragment>
+);
 
 VideoBannerView.propTypes = {
     suggestedUser: PropTypes.string.isRequired,

31b92a4586b692ae4dde2d90c363ac1db9cd78b1
chrislmy <zcabmyl@ucl.ac.uk>
2019-07-15 22:46:21 +0100

    Updates

---
+++ precache-manifest.ef3a89f0d21fa081af2285f382ae025c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42659c6efece450aa56e",
+    "url": "/online-jukebox/static/js/runtime~main.42659c6e.js"
+  },
+  {
+    "revision": "e66e777848f3ebfff135",
+    "url": "/online-jukebox/static/js/main.e66e7778.chunk.js"
+  },
+  {
+    "revision": "d48d42c3ea83cac2a255",
+    "url": "/online-jukebox/static/js/1.d48d42c3.chunk.js"
+  },
+  {
+    "revision": "e66e777848f3ebfff135",
+    "url": "/online-jukebox/static/css/main.431fa885.chunk.css"
+  },
+  {
+    "revision": "d48d42c3ea83cac2a255",
+    "url": "/online-jukebox/static/css/1.05042a6d.chunk.css"
+  },
+  {
+    "revision": "ab9ff3c0ff4dbfbbf1ec6bab1fc65fb6",
+    "url": "/online-jukebox/index.html"
+  }
+];
